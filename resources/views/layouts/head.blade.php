431a10254fe4bc251742f1c5a9d130d26b0d52c8
leleballack <emanuele.ballacci@gmail.com>
2021-02-09 08:59:46 +0000

    :bug: Removed file by mistake :package: Update dependencies

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,12 +1,17 @@
 <meta charset="utf-8">
-<meta name="author" content="{{ Config::get('const.software_company') }}">
-<meta name="description" content="{{ Config::get('const.software_name') }} - @yield('title')">
+<meta name="author" content="{{ config('const.software_company') }}">
+<meta name="description" content="{{ config('const.software_name') }} - @yield('title')">
 <meta name="MobileOptimized" content="width">
 <meta name="HandheldFriendly" content="true">
 <meta class="viewport" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, shrink-to-fit=no">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="robots" content="nofollow, noindex">
 <meta name="theme-color" content="#0066CC">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>{{ config('const.software_name') }} - @yield('title')</title>
 
 @yield('meta')
 
