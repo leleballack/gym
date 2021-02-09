ddc764d83bab6218e7a89502da29bcc1294e399d
leleballack <emanuele.ballacci@gmail.com>
2021-02-09 07:08:31 +0000

    :package: Laravel Mix

--- resources/views/exercises/index.blade.php
+++ resources/views/exercises/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.basic')
+@extends('layouts.app')
 
 @section('title', 'Exercise List')
 
@@ -8,65 +8,64 @@
 
 @section('content')
 
-    <div id="wrapper">
 
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
-                    <a href="{{ url('/') }}">
-                        {{ Config::get('const.software_name') }}
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('exercises.index') }}">Exercises List</a>
-                </li>
-                <li>
-                    <a href="{{ route('exercises.create') }}">Create Exercise</a>
-                </li>
-                <li>
-                    <a href="#">Create Workout</a>
-                </li>
-                <li>
-                    <a href="#">Assign Workout</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /Sidebar -->
+{{--        <!-- Sidebar -->--}}
+{{--        <div id="sidebar-wrapper">--}}
+{{--            <ul class="sidebar-nav">--}}
+{{--                <li class="sidebar-brand">--}}
+{{--                    <a href="{{ url('/') }}">--}}
+{{--                        {{ Config::get('const.software_name') }}--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="{{ route('exercises.index') }}">Exercises List</a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="{{ route('exercises.create') }}">Create Exercise</a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="#">Create Workout</a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="#">Assign Workout</a>--}}
+{{--                </li>--}}
+{{--            </ul>--}}
+{{--        </div>--}}
+{{--        <!-- /Sidebar -->--}}
 
-        <!-- List -->
-        <table class="table">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>First</th>
-                <th>Last</th>
-                <th>Handle</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <th>1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th>2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th>3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-            </tr>
-            </tbody>
-        </table>
-        <!-- /List -->
+{{--        <!-- List -->--}}
+{{--        <table class="table">--}}
+{{--            <thead>--}}
+{{--            <tr>--}}
+{{--                <th>#</th>--}}
+{{--                <th>First</th>--}}
+{{--                <th>Last</th>--}}
+{{--                <th>Handle</th>--}}
+{{--            </tr>--}}
+{{--            </thead>--}}
+{{--            <tbody>--}}
+{{--            <tr>--}}
+{{--                <th>1</th>--}}
+{{--                <td>Mark</td>--}}
+{{--                <td>Otto</td>--}}
+{{--                <td>@mdo</td>--}}
+{{--            </tr>--}}
+{{--            <tr>--}}
+{{--                <th>2</th>--}}
+{{--                <td>Jacob</td>--}}
+{{--                <td>Thornton</td>--}}
+{{--                <td>@fat</td>--}}
+{{--            </tr>--}}
+{{--            <tr>--}}
+{{--                <th>3</th>--}}
+{{--                <td>Larry</td>--}}
+{{--                <td>the Bird</td>--}}
+{{--                <td>@twitter</td>--}}
+{{--            </tr>--}}
+{{--            </tbody>--}}
+{{--        </table>--}}
+{{--        <!-- /List -->--}}
 
-    </div>
+
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,15 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ Config::get('const.software_name') }} - @yield('title')</title>
+        @section('head')
+            @include('layouts.head')
+        @show
 
     <!-- Scripts -->
+    @section('scripts')
+        @include('layouts.scripts')
+    @show
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
@@ -24,7 +30,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ Config::get('const.software_name') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -33,7 +39,10 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                        <li>4</li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -45,7 +54,7 @@
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-                            
+
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
@@ -76,7 +85,17 @@
         </nav>
 
         <main class="py-4">
-            @yield('content')
+            <div id="wrapper">
+                <h1 class="page-title">@yield('title')</h1>
+
+                <div class="content-page">
+                    <div class="content">
+                        <div class="container-fluid">
+                            @yield('content')
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
     </div>
 </body>
